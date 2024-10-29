c21f162adf94e7748b45e3d5262230f399309e5d
MiguelAngelJimenezBuenahora <186082664+MiguelAngelJimenezBuenahora@users.noreply.github.com>
2024-10-29 12:22:29 -0500

    Solución del ejercicio "Saludo"

---
+++ main.py
@@ -0,0 +1,6 @@
+#Escriba un programa que pida al usuario que escriba su nombre, y lo salude llamándolo por su nombre.
+#Ingrese su nombre: Perico
+#Hola, Perico
+name = input("Insert your first name please: ")
+print(f"""Hello, {name}""")
+
