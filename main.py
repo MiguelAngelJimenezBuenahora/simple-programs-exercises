62b8850d4c3f5459ccf701c7e83b8b7317e72946
MiguelAngelJimenezBuenahora <186082664+MiguelAngelJimenezBuenahora@users.noreply.github.com>
2024-10-29 15:03:17 -0500

    Solución del ejercicio "Qué nota necesito"

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-#Escriba un programa que entregue la parte decimal de un número real ingresado por el usuario.
+#Un alumno desea saber que nota necesita en el tercer certamen para aprobar un ramo.
 
-#Ingrese un numero: 4.5
-#0.5
+#El promedio del ramo se calcula con la siguiente formula.
+#NC=(C1+C2+C3)/3
+#NF=NC⋅0.7+NL⋅0.3
 
-#Ingrese un numero: -1.19
-#0.19
-realNumber = (float)(input("Please insert a number: "))
-if realNumber < 0:
-    numberfordecimal= realNumber*-1
-print(round((numberfordecimal-(int(numberfordecimal))),2))
+#Donde NC
+#es el promedio de certámenes, NL el promedio de laboratorio y NF la nota final.
+
+#Escriba un programa que pregunte al usuario las notas de los dos primeros certamen y la nota de laboratorio, y muestre la nota que necesita el alumno para aprobar el ramo con nota final 60.
+
+#Ingrese nota certamen 1: 45
+#Ingrese nota certamen 2: 55
+#Ingrese nota laboratorio: 65
+#Necesita nota 72 en el certamen 3
+
+nFirstCertamen = (int)(input("Please insert the score of the first certamen: "))
+nSecondCertamen = (int)(input("Please insert the score of the second certamen: "))
+nl= (int)(input("please insert the score of the laboratory: "))
+
+nc = ((60 - nl * 0.3) / 0.7)
+print(f"""Need a score of {((int)(3*nc-nFirstCertamen-nSecondCertamen))} in the third certamen for pass in NF with 60""")
