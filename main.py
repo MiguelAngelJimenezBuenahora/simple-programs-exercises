d90625b0e818b5184e66b41057185711ae6a6529
MiguelAngelJimenezBuenahora <186082664+MiguelAngelJimenezBuenahora@users.noreply.github.com>
2024-10-29 13:10:14 -0500

    Solución del ejercicio "Conversión de unidades de longitud"

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-#Escriba un programa que calcule el promedio de 4 notas ingresadas por el usuario:
-#Primera nota: 55
-#Segunda nota: 71
-#Tercera nota: 46
-#Cuarta nota: 87
-#El promedio es: 64.75
-import math
-print("In the current text insert the 4 notes in order: ")
-FirstNote = (float)(input("Insert the first note: "))
-SecondNote = (float)(input("Insert the second note: "))
-ThirdNote = (float)(input("Insert the third note: "))
-FourNote = (float)(input("Insert the quarter note: "))
-print(f"""the grade average is: {round(((FirstNote+SecondNote+ThirdNote+FourNote)/4),2)}""")
+#Escriba un programa que convierta de centímetros a pulgadas. Una pulgada es igual a 2.54 centímetros.
+#Ingrese longitud: 45
+#45 cm = 17.7165 in
+
+#Ingrese longitud: 13
+#13 cm = 5.1181 in
+
+length = (float)(input("Please insert the lenght on cm to convert to in: "))
+print(f"""{length} cm = {round((length/2.54),4)} in""")
+
+
