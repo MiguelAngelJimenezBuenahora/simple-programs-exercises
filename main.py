c8102da5545c81c988a0d1d39ebcea045489f6cb
MiguelAngelJimenezBuenahora <186082664+MiguelAngelJimenezBuenahora@users.noreply.github.com>
2024-10-29 12:45:34 -0500

    Solución del ejercicio "Círculos"

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-#Escriba un programa que pida al usuario que escriba su nombre, y lo salude llamándolo por su nombre.
-#Ingrese su nombre: Perico
-#Hola, Perico
-name = input("Insert your first name please: ")
-print(f"""Hello, {name}""")
-
+#Escriba un programa que reciba como entrada el radio de un círculo y entregue como salida su perímetro y su área:
+#Ingrese el radio: 5
+#Perimetro: 31.4
+#Área: 78.5
+import math
+radio = (float)(input("Insert the radius to circle: "))
+print(f"""Perimetro: {round((2*(math.pi)*radio),1)}
+Área: {round(((math.pi)*(radio*radio)),1)}""")
